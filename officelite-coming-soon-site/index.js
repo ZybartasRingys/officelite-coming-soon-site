43547c3c7e6719b2dbe9e010a2e51c25fc4e9bac
Zybartas Ringys <73960240+ZybartasRingys@users.noreply.github.com>
2021-11-06 11:19:58 +0200

    Add files via upload

---
+++ officelite-coming-soon-site/index.js
@@ -0,0 +1,13 @@
+const emailInput = document.getElementById("email");
+const submitbtn = document.getElementById("submit");
+
+const re =
+  /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+submitbtn.addEventListener("click", emailValidation);
+
+function emailValidation(email) {
+  if (emailInput.value.match(re)) {
+    emailInput.innerHTML = "Geras email";
+  }
+}
